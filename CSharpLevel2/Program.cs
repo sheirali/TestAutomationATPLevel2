a7ff951018f58b94e01ebb6b2a6ee41f657898ca
angelov.st.anton <angelov.anton@yahoo.com>
2020-10-14 14:48:30 +0300

    add new example project

---
+++ CSharpLevel2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CSharpLevel2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
